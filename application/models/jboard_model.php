4a5d8c6899a28a4015681e6bc5819783c104bfc1
junphp <gom3572@hotmail.com>
2016-05-01 15:36:46 -0400

    Added files via upload

--- application/models/jboard_model.php
+++ application/models/jboard_model.php
@@ -11,19 +11,48 @@ class Jboard_model extends CI_Model {
 
   }
 //all article call and store
-	 function mydata($limit,$start)
-	{
-    //$this->db->limit($limit,$start);
-    $query = $this->db->query('SELECT*FROM jdmain ORDER BY id desc',$limit,$start);
+	 function mydata($table_name,$limit,$offset){
 
-      if($query->num_rows()>0){
+    $this->db->order_by('id','desc');
 
-        return  $query->result();
-      }else{
+    $query = $this->db->get($table_name,$limit,$offset);
 
-        return FALSE;
+
+      if( $query->num_rows() > 0 )
+          return $query->result();
+      else
+          return FALSE;
+	  }
+
+    function prebtn($id,$table_name){
+
+      $this->db->order_by('id','desc');
+      $this->db->where('id<',$id);
+      //$this->db->or_where('id>',0);
+      return $query = $this->db->get($table_name,1)->row();
+
+          }
+
+    function nextbtn($id,$table_name){
+
+      //$this->db->order_by('id','desc');
+      $this->db->where('id>',$id);
+      //$this->db->or_where('id>',0);
+      //return $query = $this->db->get($table_name,1)->row();
+
+      $query = $this->db->get($table_name,1);
+
+      if($query->num_rows() > 0){
+          return $query->row();
+        }else{
+          return 0;
+        }
       }
-	}
+
+    function last($table_name){
+        $this->db->order_by('id','desc');
+        return $this->db->get($table_name)->row();
+    }
 
 //total article count
   function totla_data(){
@@ -31,57 +60,262 @@ class Jboard_model extends CI_Model {
     return $this->db->count_all_results('jdmain');
   }
 //single date call and store
-  function singledata($id){
-    return $this->db->get_where('jdmain',array('id'=>$id))->row();
+  function singledata($table_name,$id){
+    return $this->db->get_where($table_name,array('id'=>$id))->row();
+  }
+// board list show comment total
+  function comment_data($table_name,$id){
+    return  $this->db->get_where($table_name,array('board_id'=>$id))->result();
   }
 
-//new article
-  function add($title,$description){
+  /*function get_last_id(){
+      $this->db->order_by("id", "desc");
+      $this->db->select('id');
+      $query=$this->db->get('jdmain')->result_array();
+      return $query;
+  }*/
+  /*function child_comment_data($id){
+    //$this->db->from('child_comment');
+    return $this->db->get_where('child_comment',array('board_id'=>$id))->result_array();
+    //$this->db->where(array('parent_id'=>))
+    /*$this->db->where('board_id',$board_id);
+    $this->db->where('parent_id',$parent_id);
+    $query->$this->db->get('child_comment');
+    if ($query->num_rows() !== 0) {
+      return $query->result_array();
+    }else{
+      return FALSE;
+    }
+  }*/
+
+
+
+  function total_comments($table_name,$id){
+
+    $this->db->like('board_id',$id);
+    $this->db->from($table_name);
+    return $this->db->count_all_results();
+  }
+
+//current article posted
+  function get_latest($table_name){
+
+    $this->db->order_by('id','desc');
+    $this->db->limit('5');
+    return $query = $this->db->get($table_name)->result();
+  }
+
+  //current popular article with higher like vote received
+  function get_popular($table_name){
+
+    $this->db->order_by('vote','desc');
+    $this->db->limit('5');
+    return $query = $this->db->get($table_name)->result();
+  }
+
+
+  //add new article to board
+  function add($table_name,$user_info){
     $this->db->set('created','NOW()',false);
-    $this->db->insert('jdmain',array(
-      'title'=>$title,
-      'description'=>$description,
+    $this->db->insert($table_name,array(
+      'title'=>$user_info['title'],
+      'user_id'=>$user_info['user_id'],
+      'user_name'=>$user_info['user_name'],
+      'description'=>$user_info['description']
     ));
     return $this->db->insert_id();
   }
+//add comment for indivisual data
+  function add_comment($table_name,$comment_array){
+    $this->db->set('created','NOW()',false);
+    $this->db->insert($table_name,array(
+        'board_id'=>$comment_array['board_id'],
+        'user_id'=>$comment_array['user_id'],
+        'user_name'=>$comment_array['user_name'],
+        'article'=>nl2br($comment_array['article']),
+    ));
+    return  $this->db->insert_id();
+  }
 
-  function updatedb($id,$data){
+//threaded comment
+  function add_child_comment($child_comment_array,$parent_id){
+      $this->db->set('created','NOW()',false);
+      $this->db->insert('child_comment',array(
+          'board_id'=>$child_comment_array['board_id'],
+          'user_id'=>$child_comment_array['user_id'],
+          'user_name'=>$child_comment_array['user_name'],
+          'article'=>nl2br($child_comment_array['article']),
+          'parent_id'=>$parent_id
+      ));
+      return  $this->db->insert_id();
+  }
 
-              $this->db->where('id', $id);
-              $this->db->update('jdmain', $data);
+  //for indivisual page , total comment count
+  function update_comment($table_name,$id){
+      $this->db->set('tcomment','tcomment+1',FALSE);
+      $this->db->where('id',$id);
+      $this->db->update($table_name);
+
+  }
+
+
+//modify article
+  function updatedb($board_name,$id,$data){
+
+              $this->db->where('id',$id);
+              $this->db->update($board_name,$data);
        }
 
 //article delete
-  function del_user_id($id){
+  function del_user_id($table_name,$id){
     $this->db->where('id',$id);
-    $delete=$this->db->delete('jdmain');
+    $delete=$this->db->delete($table_name);
     if($delete){
       return true;
     }else{
       return false;
     }
   }
+
 //article view count
-    function updateview($id){
+    function updateview($table_name,$id){
       $this->db->set('view','view + 1',FALSE);
       $this->db->where('id',$id);
-      $this->db->update('jdmain');
+      $this->db->update($table_name);
     }
 
-    function page_cut($limit, $id){
 
-      $this->db->limit($limit);
-      $this->db->where('id', $id);
-      $query = $this->db->get("jdmain");
-      if ($query->num_rows() > 0) {
-foreach ($query->result() as $row) {
-$data[] = $row;
-}
+//recived search data
+    function search_record_count($terms) {
 
-return $data;
-}
-return false;
-}
+      $this->db->like('title', $terms);
+      $this->db->or_like('description', $terms);
+      $this->db->from('jdmain');
+      $this->db->from('picture');
 
+      return $this->db->count_all_results();
+
+    }
+
+    function search($terms)
+    {
+      $this->db->order_by('id','desc');
+      $this->db->like('title', $terms);
+      $this->db->or_like('description', $terms);
+      $query=$this->db->get('wall');
+
+      if($query->num_rows()>0){
+        return $query->result();
+
+      }else{
+        return false;
+      }
+    }
+
+
+    function like_update_vote($table_name,$id){
+
+        $this->db->set('vote','vote + 1',FALSE);
+        $this->db->where('id',$id);
+        $this->db->update($table_name);
+        return $this->db->affected_rows();
+
+    }
+//insert row if user not voting yet
+    function exist_vote($table_name,$user_info){
+
+
+        $this->db->insert($table_name,array('user_id'=>$user_info['user_id'], 'board_id'=>$user_info['board_id']));
+        return  $this->db->insert_id();
+    }
+//check table for existing user voting
+    function is_vote($table_name,$user_id,$board_id){
+
+        $this->db->where('user_id',$user_id);
+        $this->db->where('board_id',$board_id);
+        $query=$this->db->get($table_name);
+
+        if($query->num_rows() > 0){
+          return FALSE;
+        }else{
+          return TRUE;
+        }
+    }
+
+    function dis_like_update_vote($table_name,$id){
+
+        $this->db->set('dis_vote','dis_vote + 1',FALSE);
+        $this->db->where('id',$id);
+        $this->db->update($table_name);
+        return $this->db->affected_rows();
+    }
+
+    function main1_data(){
+      $this->db->order_by('id','desc');
+      $this->db->limit(10);
+      $query = $this->db->get('wall');
+
+
+        if( $query->num_rows() > 0 ){
+            return $query->result();
+        }else{
+            return FALSE;
+          }
+      }
+      function main2_data(){
+        $this->db->order_by('id','desc');
+        $this->db->limit(10);
+        $query = $this->db->get('picture');
+
+
+          if( $query->num_rows() > 0 ){
+              return $query->result();
+          }else{
+              return FALSE;
+            }
+        }
+
+        function main3_data(){
+          $this->db->order_by('id','desc');
+          $this->db->limit(10);
+          $query = $this->db->get('info');
+
+
+            if( $query->num_rows() > 0 ){
+                return $query->result();
+            }else{
+                return FALSE;
+              }
+          }
+      //high vote posting for last week
+        function wbest($table_name){
+
+          $this->db->order_by('id','desc');
+          $this->db->limit(5);
+          $query=$this->db->query("select * from " .$table_name. " where WEEKOFYEAR(created)=WEEKOFYEAR(NOW())-1 ORDER BY id DESC LIMIT 6;");
+          return $query->result();
+
+        }
+
+          function list_bottom($table_name,$limit,$offset){
+            //$this->db->order_by('id','desc');
+            $query = $this->db->get($table_name,$limit,$offset);
+
+
+              if( $query->num_rows() > 0 )
+                  return $query->result();
+              else
+                  return FALSE;
+        	  }
+
+          function count($table_name,$id){
+
+            //$this->db->where('id>=',$id);
+            $query = $this->db->query('SELECT * FROM ' .$table_name.' where id <= '.$id)->num_rows();
+
+            //$query->num_rows();
+
+            return $query;
+          }
 
 }
--- application/models/user_model.php
+++ application/models/user_model.php
@@ -11,6 +11,7 @@ class User_model extends CI_Model {
   }
 
   function add_user($option){
+      $this->db->set('nickname',$option['nickname']);
       $this->db->set('email',$option['email']);
       $this->db->set('password',$option['password']);
       $this->db->set('created','NOW()',false);
@@ -19,9 +20,63 @@ class User_model extends CI_Model {
       return $result;
   }
 
+  function login($email,$password){
+
+          $this->db->from('user');
+          $this->db->where('email',$email);
+          $this->db->where('password',md5($password));
+          $login=$this->db->get()->result();
+        }
+
+  function read_user_information($email){
+
+      $condition ="email"."'".$email."'";
+      $this->db->select('*');
+      $this->db->from('user');
+      $this->db->where($condition);
+      $this->db->limit(1);
+
+      $query=$this->db->get();
+
+      if($query->num_rows()==1){
+
+          return $query->result();
+      }else{
+
+        return FALSE;
+      }
+  }
+
+  function check_user_exist($user){
+      $this->db->where('email',$user);
+      $query=$this->db->get('user');
+      if($query->num_rows()>0){
+        return true;
+      }else{
+        return FALSE;
+      }
+  }
+
+  function check_username_exist($user){
+      $this->db->where('nickname',$user);
+      $query=$this->db->get('user');
+      if($query->num_rows()>0){
+        return true;
+      }else{
+        return FALSE;
+      }
+  }
+
   function getByEmail($option){
-    $result = $this->db->get_where('user',array('email'=>$option['email']))->row();
-    return $result;
+    $result = $this->db->get_where('user',array('email'=>$option['email']));
+
+    if($result->num_rows() > 0){
+      return $result->row();
+    }else{
+      //$this->form_validation->set_message('getByEmail','The email address already exist. Please try again');
+      return false;
+    }
+
 
   }
 }
